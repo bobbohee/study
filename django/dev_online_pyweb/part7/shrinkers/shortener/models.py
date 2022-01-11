ee38ca4467132c5cc754a805e2d1fa5bc153e562
bobbohee <parkbohee0705@gmail.com>
2022-01-11 12:32:47 +0900

    [ADD] dev_online_pyweb: 마이그레이션(migration)

--- django/dev_online_pyweb/part7/shrinkers/shortener/models.py
+++ django/dev_online_pyweb/part7/shrinkers/shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20) # max_length를 지정하지 않으면 오류 발생
+    price = models.IntegerField()
+    updated_at = models.DateField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
