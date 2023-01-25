63a9729d064579ba9dc376e65ca876a9f4d0e3a1
bobbohee <parkbohee0705@gmail.com>
2023-01-25 09:30:38 +0900

    [ADD] dart_for_beginners: #2.0 Basic Data Types

---
+++ dart/dart_for_beginners/2_data_types/2_0_basic_data_types.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Dart는 객체지향언어로 모든 자료형은 object로 이루어져 있다
+  String name = 'parkbohee';
+  bool alive = true;
+  int age = 23;
+  double money = 69.99;
+
+  // num 자료형은 integer / double 자료형을 같이 할당할 수 있다.
+  num x = 12;
+  x = 1.1;
+}
