4cd861b4fed37d55dd0c18c631f7b60d32f7248f
bobbohee <parkbohee0705@gmail.com>
2023-01-31 18:00:11 +0900

    [ADD] dart_for_beginners: #4.4 Recap

---
+++ dart/dart_for_beginners/4_classes/4_4_recap.dart
@@ -0,0 +1,45 @@
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  Player.fromJson(Map<String, dynamic> playerJson) :
+    name = playerJson["name"],
+    xp = playerJson["xp"],
+    team = playerJson["team"];
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var apiData = [
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "dal",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
+}
